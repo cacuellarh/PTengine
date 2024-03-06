676363d1c58bf075ac14a1a5ad0e3e87042b5efd
ciblpaladin <camilotedi@gmail.com>
2024-03-05 19:45:07 -0500

    asd

--- screen/static/JS/cut_image.js
+++ screen/static/JS/cut_image.js
@@ -1,6 +1,6 @@
-const base_url = "http://127.0.0.1:8000/api/"
+// const base_url = "http://127.0.0.1:8000/api/"
 
-//const base_url = "http://195.35.14.162:8002/api/"
+const base_url = "http://195.35.14.162:8002/api/"
 
 //1 - Boton para cortar precio
 const btn = document.getElementById("btn_cropp")
